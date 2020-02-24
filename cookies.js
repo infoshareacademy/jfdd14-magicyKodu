97905816160ba7116ece549b471839f08195b266
kamil-czr <k.czarnocki@gmail.com>
2020-02-24 15:17:46 +0100

    last updates

--- cookies.js
+++ cookies.js
@@ -1,5 +1,3 @@
-
-
 const cookieBanner = document.querySelector(".cookie-banner");
 const cookieBtn = document.querySelector(".cookieBtn");
 
@@ -14,38 +12,6 @@ function checkCookies() {
     if (localStorage.getItem("cookie") === 'true') {
         closeButton();
     }
-
 }
 
 checkCookies();
-
-/*
-localStorage.setItem("lastname", "Smith");
-// Retrieve
-document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-
-
-
-    // pobieram mój przycisk utworzony kilka linijek powyżej, szukając button w elemencie LI
-    const btn = li.querySelector('button');
-    
-    //przypisuję do niego akcję kliknięcia, która ma na celu usuwanie elementu
-    cookieBtn.addEventListener("click", removeElementFromList);
-    
-
-
-// funkcja mająca na celu usuwanie elementu z drzewa DOM
-const removeElementFromList = function (event) {
-    // event.target jest to mój element, który został kliknięty, w tym przypadku jest to mój BUTTON
-    const pressedBtn = event.target;
-
-    // Pobieram jego rodzica, którym jest LI, w celu jego skasowania
-    const li = pressedBtn.parentElement;
-
-    // usuwam li  DOM
-    li.remove();
-
-    // aktualizuje stan w localstorage
-    saveToLocalStorage();
-}
-*/
