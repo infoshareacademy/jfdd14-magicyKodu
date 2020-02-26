ace51fa236b59b0c5719f6f06ce96ed8a67d7ade
Dawid <you@example.com>
2020-02-26 16:27:09 +0100

    git added stones and bananas moves

--- Runner Game/stone.js
+++ Runner Game/stone.js
@@ -1,52 +1,13 @@
-// class Stone extends Properties {
-//     width = 50;
-//     height = 50;
-//     color= "gray";
-//     speed = 50;
-//     constructor(x, y){
-//         super(x, y);
-//     }
-// }
-
-// const stone = new Stone(500, 550);
-// console.log(stone);
-
-// move(){
-//     startX += speed;
-// }
-// function animationFrame(){
-//     ctx.clearRect(0,0,800,800);
-
-//     stone.init();
-//     stone.move();
-// }
-
-class Stone{
-    constructor(startX,startY){
-        this.witdth = scale*2;
-        this.height = scale*3;
-        this.speed = scale;
-        this.startX = startX;
-        this.startY = startY;
+class Stone extends Properties {
+    width = 50;
+    height = 50;
+    color= "gray";
+    speed = 10;
+    constructor(x, y){
+        super(x, y);
     }
 
-    init(){
-    ctx.fillStyle = "gray"
-    ctx.fillRect(this.startX,this.startY,this.witdth,this.height);
-    }
-    move(){
-        this.startX -= this.speed;
+    move() {
+        this.x -= this.speed;
     }
 }
-const posX = 1100
-const posY = 400
-
-const stone = new Stone(posX,posY);
-
-function animationFrame(){
-    ctx.clearRect(0,0,1200,1200);
-    stone.init();
-    stone.move();
-}
-
-const refreshFrame = setInterval(animationFrame,600); 
