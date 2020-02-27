42252285f815405daec5dcce21fd6e7f8b576e4e
kamil-czr <k.czarnocki@gmail.com>
2020-02-27 15:47:14 +0100

    Kaj edit

--- Runner Game/runner.js
+++ Runner Game/runner.js
@@ -4,6 +4,7 @@ class Runner extends Properties {
     speed = 10;
     gravity = 10;
     color = "black";
+    isRun = true;
     constructor(x, y) {
         super(x, y);
     }
@@ -13,35 +14,27 @@ class Runner extends Properties {
                 this.y -= this.speed ;
                 if (this.y < 200) {
                  this.speed = -this.speed ;
-                } if (this.y > 400) {
+                } if (this.y > 400 && this.isRun) {
                     this.speed = 0;
-                }
-        
-    
+                    this.isRun = false;
+                    clearInterval(moveInterval);
+                } 
+      
     }
+
+    resetSpeed = () => {console.log('rs'); this.speed=10;}
 };
-
-
-
 
 const runner = new Runner(100, 400);
 console.log(runner);
 
+let moveInterval;
 
 
-function setup() {
-    // Dodanie event listenera na wciśnięcie przycisku
-    window.addEventListener("keydown", event => runner.move(event.key));
+window.addEventListener("keydown", event => {runner.resetSpeed(); runner.move(event.key); moveInterval = window.setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    runner.print();
+    runner.move();
+    runner.isRun = true;
 
-
-    // Nasza "klatka" gry, elementy które mają wykonać się cyklicznie są wkładane tutaj
-    window.setInterval(() => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        runner.print();
-        runner.move();
-        
-    },50);
-}
-
-window.addEventListener('keydown', setup)
-// setup();
+}, 50); });
