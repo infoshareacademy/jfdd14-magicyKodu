4b66c0bb58e3c68ce262c68c07fe9a887af168ce
karo <karo.szl@gmail.com>
2020-02-27 15:02:44 +0100

    Banana generator added

--- Runner Game/game.js
+++ Runner Game/game.js
@@ -1,70 +1,53 @@
 const canvas = document.getElementsByClassName("canvas")[0];
 const ctx = canvas.getContext("2d"); 
 const scale = 20; 
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+const btn = document.querySelector("button");
 
-
-class Game {
-    x = 0;
-    y = 0;
-    
-    constructor(width, height){
-        this.width = canvas.width;
-        this.height = canvas.height;
-        // this.x = 0;
-        // this.y = 0;
-        this.canvas = canvas;
-        this.ctx = canvas.getContext("2d"); 
-        this.rows = canvas.height/scale;
-        this.columns = canvas.width/scale;
-
+class Banana extends Properties{
+    x = 1200;
+    y = 300;
+    width = 10;
+    height = 30;
+    color = "yellow";
+    constructor(x, y){
+        super(x, y);
     }
 
-    updateBananas = () => {
-        
-        // if (banana.x === 600) {
-            
-        //     // bananas.push(banana);
-        //     // console.log(bananas);
-        //     console.log("lalalalala");
-            
-        // }
+    move = (speed) => {
+        this.x -= speed;
     }
-
-    print = () => {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    
 }
-
-const game = new Game();
 
 let bananas = [];
 
-function newBanana() {
-    const min = 2;
-    const max = 5;
-    let rand = Math.floor(Math.random() * (max - min + 1) + min); 
-    bananas.push(banana);
-    banana.move(5);
-    banana.print();
-   console.log(bananas);
-    setTimeout(newBanana, rand * 1000);
-  }
-  
-  
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-function setup() {
-    newBanana(); 
-    window.setInterval(() => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);  
-         
-        
-        // game.updateBananas();     
-          
-        floor.print();     
+function newBananas() {
+    let rand = getRandomInt(10,20);
+    bananas.push(new Banana);
+    setTimeout(newBananas, rand * 500);
+}
+  
+function setUp() {
+    
+    window.setInterval ( () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        floor.print();   
+        bananas.forEach( el => {
+            el.print();
+            el.move(5);
+        });
     }, 50);
     
 }
 
-setup();
+btn.addEventListener("click", setUp);
+newBananas();
+// setUp();
+
