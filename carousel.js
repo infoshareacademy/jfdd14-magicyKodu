d58dd5a27b663f492d9204c68dd70603828faf65
Karolina <karolina.szlenk@gmail.com>
2020-03-02 20:07:10 +0100

    Comments added

--- carousel.js
+++ carousel.js
@@ -4,31 +4,38 @@ const arrowPrev = document.querySelector("#hero__arrowPrev");
 const dot1 = document.querySelector("#hero__dot1");
 const dot2 = document.querySelector("#hero__dot2");
 const dot3 = document.querySelector("#hero__dot3");
-// const dots = document.querySelectorAll(".hero__dot");
 
+//array from slides/images
 const arr = [1, 2, 3];
 let num = 1;
+
+//last number in array
 let lastNum = arr[arr.length-1];
 
+//painting first dot when website starts
 paintDot(num);
 
+//changing silde/image when click on right arrow
 function changeImgToRight() {
     (num < lastNum ) ? num++ : num = 1;
     heroSection.classList = `hero hero${num}`;
     paintDot(num);
 }
 
+//changing silde/image when click on left arrow
 function changeImgToLeft() {
     (num === 1) ? num = lastNum : num--;
     heroSection.classList = `hero hero${num}`;
     paintDot(num);
 }
 
+//changing silde/image when click on dots
 function changeImg(num){
     heroSection.classList = `hero hero${num}`;
     paintDot(num);    
 }
 
+//painting current dot
 function paintDot(num){
     if (num === 1){
         dot1.classList.add("active");
