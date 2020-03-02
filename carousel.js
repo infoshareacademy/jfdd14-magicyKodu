5cac9db562a9d7cd4ce11ccc6c25e7fa40836b44
Karolina <karolina.szlenk@gmail.com>
2020-03-02 16:53:32 +0100

    paintDot added and  code optimised

--- carousel.js
+++ carousel.js
@@ -4,63 +4,51 @@ const arrowPrev = document.querySelector("#hero__arrowPrev");
 const dot1 = document.querySelector("#hero__dot1");
 const dot2 = document.querySelector("#hero__dot2");
 const dot3 = document.querySelector("#hero__dot3");
+// const dots = document.querySelectorAll(".hero__dot");
 
 const arr = [1, 2, 3];
-// let num = 1;
+let num = 1;
 let lastNum = arr[arr.length-1];
 
-myFunc();
+paintDot(num);
 
 function changeImgToRight() {
     (num < lastNum ) ? num++ : num = 1;
     heroSection.classList = `hero hero${num}`;
+    paintDot(num);
 }
 
 function changeImgToLeft() {
     (num === 1) ? num = lastNum : num--;
     heroSection.classList = `hero hero${num}`;
+    paintDot(num);
 }
 
-function changeImgByCircle1(){
-    num = 1;
+function change(num){
     heroSection.classList = `hero hero${num}`;
-    dot1.style.color = "#28bb76";
-    dot2.style.color = "white";
-    dot3.style.color = "white";
+    paintDot(num);    
 }
 
-function changeImgByCircle2(){
-    num = 2;
-    heroSection.classList = `hero hero${num}`;
-    dot1.style.color = "white";
-    dot2.style.color = "#28bb76";
-    dot3.style.color = "white";
+function paintDot(num){
+    if (num === 1){
+        dot1.classList.add("active");
+        dot2.classList.remove("active");
+        dot3.classList.remove("active");
+    } else if (num === 2){
+        dot1.classList.remove("active");
+        dot2.classList.add("active");
+        dot3.classList.remove("active");
+    } else {
+        dot1.classList.remove("active");
+        dot2.classList.remove("active");
+        dot3.classList.add("active");   
+    }
 }
 
-function changeImgByCircle3(){
-    num = 3;
-    heroSection.classList = `hero hero${num}`;
-    dot1.style.color = "white";
-    dot2.style.color = "white";
-    dot3.style.color = "#28bb76";
-}
+arrowNext.addEventListener("click", changeImgToRight);
+arrowPrev.addEventListener("click", changeImgToLeft);
 
 
-
-function addClickEvent(el, callback){
-    el.addEventListener("click", callback)
-}
-
-function myFunc() {
-    dot1.style.color = "#28bb76";
-}
-
-addClickEvent(arrowNext, changeImgToRight);
-addClickEvent(arrowPrev, changeImgToLeft);
-
-dot1.addEventListener("click", changeImgByCircle1);
-dot2.addEventListener("click", changeImgByCircle2);
-dot3.addEventListener("click", changeImgByCircle3);
 
 
 
