89f7f93224d3e1b6f24c5849d5d6d155d805c42c
Karolina <karolina.szlenk@gmail.com>
2020-03-03 21:19:43 +0100

    Animation frame instead of interval

--- Runner Game/game.js
+++ Runner Game/game.js
@@ -1,7 +1,6 @@
 const canvas = document.getElementsByClassName("canvas")[0];
 const ctx = canvas.getContext("2d"); 
-const scale = 10; 
-const rows = canvas.height / scale;
+const scale = 10;
 const columns = canvas.width / scale;
 const btn = document.querySelector("button");
 const result = document.querySelector(".score");
@@ -136,27 +135,27 @@ function bananaScore() {
 function check(el){
 
 }
+
+newBananas(); 
+newStones();
   
 function setUp() { 
     
-    newBananas(); 
-    newStones();
-    window.setInterval( () => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);       
-        floor.print();
-        runner.print();
-        runner.move();  
-        bananas.forEach(el => {
-            el.print();
-            el.move();                 
-        });     
-        stones.forEach( el => {
-            el.print();
-            el.move();
-        }); 
-        bananaScore();         
-    }, 30);
-    // check();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);       
+    floor.print();
+    runner.print();
+    runner.move();  
+    bananas.forEach(el => {
+        el.print();
+        el.move();                 
+    });     
+    stones.forEach( el => {
+        el.print();
+        el.move();
+    }); 
+    bananaScore();         
+
+    window.requestAnimationFrame(setUp);
 }
 
 
@@ -168,48 +167,6 @@ window.addEventListener("keydown", e => {
     }
 })
 
-btn.addEventListener("click", setUp);
+// btn.addEventListener("click", setUp);
 
-
-
-
-//----------OLD VERSION----------
-// let stones = [];
-// let bananas = [];
-
-// function updateStones(stone, index, data) {
-
-//     stone.print();
-//     stone.move();
-//     console.log(index, data.length)
-//     if (stone.x < 0) {
-//         console.log("wywalam")
-//         // stones.splice(index, 1);
-//         data[index] = null;
-//     }
-// }
-
-// let time = 0;
-
-// function getRandomFromTo(min, max) {
-//     return Math.floor(Math.random()*(max - min) + min);
-// }
-// function animationFrame(){
-//     ctx.clearRect(0,0,1200,1200);
-
-//     stones.forEach(updateStones);
-//     stones = stones.filter(function (el) { return !!el })
-
-
-//     bananas.forEach(updateStones);
-//     bananas = bananas.filter(function (el) { return !!el })
-
-
-//     if (time % getRandomFromTo(10, 10) === 0) {
-//         stones.push(new Stone(1200,400));
-//         bananas.push(new Banana(1200,300));
-//     }
-//     time ++;
-// }
-
-// const refreshFrame = setInterval(animationFrame,40); 
+window.requestAnimationFrame(setUp);
