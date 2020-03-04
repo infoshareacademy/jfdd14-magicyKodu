9f364e62784efe91637c4f0971dbc80f92c61576
Karolina <karolina.szlenk@gmail.com>
2020-03-04 20:55:09 +0100

    Images, buttons added, properties changed

--- Runner Game/game.js
+++ Runner Game/game.js
@@ -3,17 +3,24 @@ const canvas = document.getElementsByClassName("canvas")[0];
 const ctx = canvas.getContext("2d"); 
 const scale = 10;
 const columns = canvas.width / scale;
-const btn = document.querySelector("button");
-const result = document.querySelector(".score");
-let gameOver = false;
+const btn = document.querySelector(".userPanel__buttons__btnStart");
+const btnInstruction = document.querySelector(".userPanel__buttons__btnInstruction");
+const result = document.querySelector(".userPanel__points__score");
+const instruction = document.querySelector(".gameInstruction");
+const imgBanana = document.getElementById("banana");
+const imgStone = document.getElementById("stone");
+const imgRunner = document.getElementById("runner"); 
+const imgBird = document.getElementById("bird"); 
+const gameOver = false;
 
 //----------Configuration speed----------
-let bananaSpeed = 6;
-let stoneSpeed = 6;
+let bananaSpeed = 9;
+let stoneSpeed = 9;
 let bananaRandomMin = 1;
 let bananaRandomMax = 5;
 let stoneRandomMin = 1;
 let stoneRandomMax = 5;
+
 
 //----------Speed change----------
 const bananaSpeedRatio = 0.001; // speed up ratio
@@ -23,27 +30,30 @@ const ratio = 0.0001 // banana/stone frequency increment
 function startGame() {
 //----------General Class Properties----------
 class Properties {
-    constructor(x, y, width, height, color){
+    constructor(x, y, width, height, color, img){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.color = color;
+        this.img = img;
     }
     print = () => {
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        // ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }    
 }
 
 //----------Class Banana----------
 class Banana extends Properties {
     x = 1200;
-    y = 300;
-    width = 10;
-    height = 30;
+    y = 220;
+    width = 30*2.2;
+    height = 30*2.2;
     color = "yellow";
     bananaHit = false; 
+    img = imgBanana;
 
     move = (speed) => {
         this.x -= speed;
@@ -53,10 +63,11 @@ class Banana extends Properties {
 //----------Class Stone----------
 class Stone extends Properties {
     x = 1200;
-    y = 450;
-    width = 50;
-    height = 50;
+    y = 455;
+    width = 40*2.5;
+    height = 30*2.5;
     color= "gray";
+    img = imgStone;
 
     move = (speed) => {
         this.x -= speed;
@@ -65,34 +76,35 @@ class Stone extends Properties {
 
 //----------Class Runner----------
 class Runner extends Properties {
-    width = 20;
-    height = 100;
+    width = 45*3.2;
+    height = 65*3.2;
     speed = 0;
     color = "black";
     score = 0;
     canJump = true;
+    img = imgRunner;
     constructor(x, y) {
         super(x, y);
     }
 
     move = () => {  
         if (controller.up && this.canJump == false) {
-            this.speed -= 40;
+            this.speed -= 50;
             this.canJump = true;
         }   
         this.speed += 1.5; // gravity
         this.y += this.speed;
         this.speed *= 0.9; // friction
         // if runner is falling below floor line
-        if (this.y > 400) {
+        if (this.y > 320) {
             this.canJump = false;
-            this.y = 400;
+            this.y = 320;
             this.speed = 0;
         }
     }
 }
 
-const runner = new Runner(100, 400);
+const runner = new Runner(100, 320);
 
 controller = {
     up:false,
@@ -102,13 +114,23 @@ controller = {
     } 
 };
 
-//----------Object floor----------
-const floor = new Properties(0, 500, 1300, 100, "brown");
+//----------Object Bird----------
+class Bird extends Properties {
+    x = 1200;
+    y = 10;
+    width = 160*0.5;
+    height = 50*0.5;
+    color= "black";
+    img = imgBird;
 
-
+    move = (speed) => {
+        this.x -= speed;
+    }
+}
 
 let bananas = [];
 let stones = [];
+let birds = [];
 
 //----------Random value----------
 function getRandomInt(min, max) {
@@ -138,6 +160,16 @@ function newStones() {
     setTimeout(newStones, rand * 1000);
 } 
 
+function newBirds() {
+    let rand = getRandomInt(bananaRandomMin, bananaRandomMax*10);
+    birds.push(new Bird());     
+    if (birds.length === 5) {
+        birds.shift();
+    }
+    setTimeout(newBirds, rand * 1000);
+}
+
+
 function catchBanana(el){
     if (el.x <= runner.x + runner.width && el.x + el.width >= runner.x // check x from both sides
         && runner.y <= el.y + el.height){ // check y just from below. You can`t jump above banana. If You would decide to do so, You would have to add another check
@@ -154,8 +186,8 @@ function catchBanana(el){
 }
 
 function collisionWithStone(el){
-    if (el.x <= runner.x + runner.width && el.x + el.width >= runner.x // check x from both sides
-        && el. y <= runner.y + runner.height){ // check y just from above. You can`t jump below stone. If You would decide to do so, You would have to add another check
+    if (el.x + 20 <= runner.x + runner.width && el.x + el.width - 20 >= runner.x // check x from both sides
+        && el. y + 40 <= runner.y + runner.height){ // check y just from above. You can`t jump below stone. If You would decide to do so, You would have to add another check
         gameOver = true;
         // console.log(game_over);
         }
@@ -172,10 +204,11 @@ function speedUp(){
 
 newBananas(); 
 newStones();
+newBirds();
+hideInstruction();
   
 function setUp() { 
     ctx.clearRect(0, 0, canvas.width, canvas.height);       
-    floor.print();
     runner.print();
     runner.move();
     bananas.forEach(el => {
@@ -188,6 +221,11 @@ function setUp() {
         el.move(stoneSpeed);
         collisionWithStone(el);
     }); 
+    birds.forEach(el => {
+        el.print();
+        el.move(bananaSpeed);                
+    }); 
+    
     speedUp();
     if (gameOver == true) {
         return;
@@ -201,8 +239,18 @@ window.addEventListener("keyup", controller.keyListener);
 window.requestAnimationFrame(setUp);
 }
 
-btn.addEventListener("click", startGame);
+//----------Instruction option----------
+function hideInstruction() {
+    instruction.classList.add("hide");
+}
 
+function showInstruction() {
+    instruction.classList.remove("hide");
+    instruction.classList.add("active");
+}
+
+btn.addEventListener("click", startGame);
+btnInstruction.addEventListener("click", showInstruction);
 
 
 //-------------OLD VERSION-------------
